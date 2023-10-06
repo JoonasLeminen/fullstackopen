471191fbe25b806309a3305f76dd9bfd08ba1300
JoonasLeminen <joonas.leminen@outlook.com>
2023-10-06 10:54:37 +0300

    Tehtävä 2.10 tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,9 +1,60 @@
 import { useState } from 'react'
 
-const PersonForm = () => {
+const PersonForm = ({ persons, newName, newNumber, setPersons, setNewName, setNewNumber }) => {
+  //console.log(persons)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+
+    //console.log(persons.map(person => person.name).includes(newName))
+    if (persons.map(person => person.name).includes(newName)) {
+      console.log('Duplicate', newName)
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
-
+      <h2>Add a new</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
     </div>
   )
 }
@@ -45,64 +96,12 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    }
-
-    //console.log(persons.map(person => person.name).includes(newName))
-    if (persons.map(person => person.name).includes(newName)) {
-      console.log('Duplicate', newName)
-      alert(`${newName} is already added to phonebook`)
-    }
-    else {
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const handleNameChange = (event) => {
-    //console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    //console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-
-  const filterName = (event) => {
-    //console.log(event.target.value)
-    //console.log(persons.map(person => person.name.includes(event.target.value)))
-  }
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <h2>Add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:
-          <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} setPersons={setPersons}
+        newName={newName} setNewName={setNewName}
+        newNumber={newNumber} setNewNumber={setNewNumber} />
       <h2>Numbers</h2>
       <PersonList persons={persons} />
     </div>

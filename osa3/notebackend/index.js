1430126360c2ecc6fa2c3d514e1fe4649acca37e
JoonasLeminen <joonas.leminen@outlook.com>
2023-10-19 09:59:03 +0300

    Tehtävä 3.1 aloitettu

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -23,6 +25,10 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!<h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
@@ -33,6 +39,33 @@ app.get('/api/notes/:id', (request, response) => {
     else {
         response.status(404).end()
     }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {

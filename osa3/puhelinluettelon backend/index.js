13a471c80aeeb9928efe7c810fbf3a31b7175ef4
JoonasLeminen <joonas.leminen@outlook.com>
2023-10-20 11:06:14 +0300

    Tehtävä 3.6 tehty

--- osa3/puhelinluettelon backend/index.js
+++ osa3/puhelinluettelon backend/index.js
@@ -1,41 +1,101 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
     },
     {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
     },
     {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
     },
     {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
 ]
-
-
 
 app.get('/', (request, response) => {
     response.send('<h1>Persons backend</h1>')
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has ${Math.max(...persons.map(n => n.id))} people listed currently`)
+    const timeDate = new Date();
+    response.send(`Phonebook has ${Math.max(...persons.map(n => n.id))} people listed currently
+    <br><br>${timeDate}`)
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+const generateID = () => {
+    const createId = Math.floor(Math.random() * 999)
+    return createId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'Name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'Number missing'
+        })
+    }
+
+    if (persons.map(person => person.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID(),
+    }
+
+    console.log(person)
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001

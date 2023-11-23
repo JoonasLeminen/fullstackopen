97628dd7e052dc4cf6e54fb950a61bf05d264e58
JoonasLeminen <joonas.leminen@outlook.com>
2023-11-23 14:05:03 +0200

    ESLint fix

--- osa3/notebackend/controllers/notes.js
+++ osa3/notebackend/controllers/notes.js
@@ -2,57 +2,57 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
+  Note.findById(request.params.id)
     .then(note => {
-        if (note) {
-            response.json
-        } else {
-            response.status(404).end()
-        }
+      if (note) {
+        response.json
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 notesRouter.post('/', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
 
-    note.save()
+  note.save()
     .then(savedNote => {
-        response.json(savedNote)
+      response.json(savedNote)
     })
     .catch(error => next(error))
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
+  Note.findByIdAndDelete(request.params.id)
     .then(() => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
+  const note = {
+    content: body.content,
+    important: body.important
+  }
 
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
     .then(updatedNote => {
-        response.json(updatedNote)
+      response.json(updatedNote)
     })
     .catch(error => next(error))
 })

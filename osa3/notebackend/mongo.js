a0a9f4957886e45b81e27310e5a6f111faf76d99
JoonasLeminen <joonas.leminen@outlook.com>
2023-11-27 15:59:04 +0200

    Osassa 4b, "lisää testejä ja backendin refaktorointia"

--- osa3/notebackend/mongo.js
+++ osa3/notebackend/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-`mongodb+srv://joonasleminen:${password}@cluster0.t1bep09.mongodb.net/noteApp?retryWrites=true&w=majority`
+`mongodb+srv://joonasleminen:${password}@cluster0.t1bep09.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -21,20 +21,20 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'Mong is Easy',
-  important: true,
+  content: 'Hieno auringonlasku tänään',
+  important: false,
 })
 
 note.save().then(
   console.log('note saved!'),
-  mongoose.connection.close()
+  //mongoose.connection.close()
 )
 
-/*Note.find({}).then(result => {
+/*
+Note.find({}).then(result => {
   result.forEach(note => {
     console.log(note)
   })
   mongoose.connection.close()
-})*/
-
-//Note.find({ important: true }).then(result => ...
+})
+*/

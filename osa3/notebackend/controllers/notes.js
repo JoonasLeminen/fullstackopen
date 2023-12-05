982cb81a3e606c5be0813ed049c87034a9d5d893
JoonasLeminen <joonas.leminen@outlook.com>
2023-12-05 15:32:49 +0200

    Osa 4c tehty

--- osa3/notebackend/controllers/notes.js
+++ osa3/notebackend/controllers/notes.js
@@ -1,39 +1,47 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response, next) => {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false
+    important: body.important == undefined ? false : body.important,
+    user: user._id
   })
 
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', async (request, response, next) => {
-    await Note.findByIdAndDelete(request.params.id)
-    response.status(204).end()
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
   const note = {
@@ -41,11 +49,8 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    response.json(note)
 })
 
 module.exports = notesRouter

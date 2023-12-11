669be1554bb701de1edbf6094448f86fcb470fbc
JoonasLeminen <joonas.leminen@outlook.com>
2023-12-11 11:25:00 +0200

    Tehtävä 4.15 tehty

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
+const User = require('../models/user')
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
@@ -14,74 +16,74 @@ beforeEach(async () => {
   //console.log(helper.initialBlogs)
 })
 
-test('blogs are returnes as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returnes as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('database contains right amount of blogs', async () => {
-  const response = await api.get('/api/blogs')
+  test('database contains right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test('blogs are identified by a string named id', async () => {
-  const response = await api.get('/api/blogs')
+  test('blogs are identified by a string named id', async () => {
+    const response = await api.get('/api/blogs')
 
-  const contentsId = response.body.map(r => r.id)
+    const contentsId = response.body.map(r => r.id)
 
-  expect(contentsId).toBeDefined()
-})
+    expect(contentsId).toBeDefined()
+  })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Tietorakenne näyttää muuttuneen',
-    author: 'JL',
-    url: 'jopas.com',
-    likes: 3,
-  }
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Tietorakenne näyttää muuttuneen',
+      author: 'JL',
+      url: 'jopas.com',
+      likes: 3,
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  const contents = blogsAtEnd.map(b => b.title)
-  expect(contents).toContain('Tietorakenne näyttää muuttuneen')
-})
+    const contents = blogsAtEnd.map(b => b.title)
+    expect(contents).toContain('Tietorakenne näyttää muuttuneen')
+  })
 
-test('a specific blog can de modified with status code 200', async () => {
-  const blogToUpdate = {
-    id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 46,
-  }
+  test('a specific blog can de modified with status code 200', async () => {
+    const blogToUpdate = {
+      id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 46,
+    }
 
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send(blogToUpdate)
-    .expect(200)
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
+      .expect(200)
 
     const blogsAtEnd = await helper.blogsInDb()
 
     expect(blogsAtEnd[0].likes).toEqual(46)
-})
+  })
 
-test('a specific blog can be deleted with status code 204', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
+  test('a specific blog can be deleted with status code 204', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
     const blogsAtEnd = await helper.blogsInDb()
 
@@ -90,6 +92,40 @@ test('a specific blog can be deleted with status code 204', async () => {
     const contents = blogsAtEnd.map(b => b.title)
 
     expect(contents).not.toContain(blogToDelete.title)
+  })
+
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({username: 'root', passwordHash})
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'jlem',
+      name: 'Joonas Leminen',
+      password: 'salainen'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
 })
 
 afterAll(async () => {
